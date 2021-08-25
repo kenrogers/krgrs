258ff665234ac424aa720d3dacfcc93ef14d795e
Ken Rogers <ken@kenrogers.co>
2021-08-24 20:53:22 -0600

    Add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Kenny, his wife Chelsea, and son Joe on a hike."
+        src="../images/family.jpeg"
+      />
     </Layout>
   );
 };
